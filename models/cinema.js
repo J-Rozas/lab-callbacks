60ce047553564b7a7330b10048bdd8aebf8c6d78
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-04 15:48:58 +0100

    Add function to filter by property for DRY

--- models/cinema.js
+++ models/cinema.js
@@ -6,8 +6,6 @@ Cinema.prototype.allFilmTitles = function () {return this.films.map(film => film
 
 Cinema.prototype.findByTitle = function (givenTitle) {return this.films.filter(film => givenTitle === film.title)[0]}
 
-Cinema.prototype.filterByGenre = function (givenGenre) {return this.films.filter(film => film.genre === givenGenre)}
-
 Cinema.prototype.moviesReleasedOnYear = function (givenYear) {return this.films.filter(film => film.year === givenYear).length === 0 ? false : true}
 
 Cinema.prototype.allMoviesOverLength = function (givenLength) {return this.films.length === this.films.filter(film => film.length === givenLength) ? true : false}
@@ -15,6 +13,6 @@ Cinema.prototype.allMoviesOverLength = function (givenLength) {return this.films
 // First map to get all the lengths, then reduce
 Cinema.prototype.totalRunningTime = function () {return this.films.map(film => film.length).reduce((previousValue, currentValue) => previousValue += currentValue, 0)}
 
-Cinema.prototype.filterByYear = function (givenYear) {return this.films.filter(film => film.year === givenYear)}
+Cinema.prototype.filterByProperty = (givenProperty, givenValue) => this.films.filter(film => film[givenProperty] === givenValue)
 
 module.exports = Cinema;

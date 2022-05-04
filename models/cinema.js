d62f7aa62a0fa4af67954de965f30cc8b69ec5a6
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-04 15:10:16 +0100

    Add function to check if there are movies from a year

--- models/cinema.js
+++ models/cinema.js
@@ -8,4 +8,6 @@ Cinema.prototype.findByTitle = function (givenTitle) {return this.films.filter(f
 
 Cinema.prototype.filterByGenre = function (givenGenre) {return this.films.filter(film => film.genre === givenGenre)}
 
+Cinema.prototype.moviesReleasedOnYear = function (givenYear) {return this.films.filter(film => film.year === givenYear).length === 0 ? false : true}
+
 module.exports = Cinema;

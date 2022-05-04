97d05637a309c28e159bb36df0f9d15a24ce6674
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-04 14:52:14 +0100

    Add function to get film by title

--- models/cinema.js
+++ models/cinema.js
@@ -4,4 +4,6 @@ const Cinema = function (films) {
 
 Cinema.prototype.allFilmTitles = function () {return this.films.map(film => film.title)}
 
+Cinema.prototype.findByTitle = function (givenTitle) {return this.films.filter(film => givenTitle === film.title)[0]}
+
 module.exports = Cinema;
